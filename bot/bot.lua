7329a5b0ac3b3d2c9f777d4ca7666ea7c7d75473
Alireza7979 <Moshfeghi206@chmail.ir>
2016-07-16 00:32:33 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {137913799},
+    sudo_users = {189814876},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
